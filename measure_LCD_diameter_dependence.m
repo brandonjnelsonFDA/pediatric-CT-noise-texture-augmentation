ef8a13f0222e81b05b5563905e4dbcca84ac4407
Brandon Nelson <bnel1201@gmail.com>
2023-06-26 18:42:22 -0700

    initial commit to diameter code

---
+++ measure_LCD_diameter_dependence.m
@@ -0,0 +1,44 @@
+base_data_folder = 'data\CCT189_peds'
+addpath(genpath('LCD_CT2'));
+observers = {LG_CHO_2D()};
+% observers = {DOG_CHO_2D()}
+% observers = {LG_CHO_2D(),...
+%              DOG_CHO_2D(),...
+%              GABOR_CHO_2D(),...
+%              };
+
+diameter_dirs = dir(fullfile(base_data_folder, 'diameter*mm'));
+n_diameters = length(diameter_dirs);
+
+ground_truth_filename = fullfile(base_data_folder, 'diameter112mm', 'fbp');
+offset = 1000;
+ground_truth = approximate_groundtruth(fullfile(base_data_folder, 'diameter112mm', 'fbp'), ground_truth_filename, offset)
+ground_truth = mhd_read_image(ground_truth_filename) - offset;
+
+for diam_idx=1:n_diameters
+    diameter_dir=diameter_dirs(diam_idx).name;
+    diameter = regexp(diameter_dir, '\d+', 'match'); diameter = str2num(diameter{:});
+    recons = dir(fullfile(base_data_folder, diameter_dir));
+    n_recons = length(recons);
+    for recon_idx=3:n_recons
+      recon = recons(recon_idx).name;
+      res = measure_LCD(fullfile(diameter_dir, recon), observers, ground_truth, offset, nreader, pct_split, seed_split);
+      if is_octave
+       res.recon = recon;
+       res.diameter = diameter;
+      else
+        res.recon(:) = recon;
+        res.diameter(:) = diameter;
+      end
+      if i == 3
+        res_table = res
+      end
+      if is_octave
+        res_table = vertcat(res_table, res);
+      else
+        res_table = cat(1, res_table, res);
+      end
+    end
+end
+
+% where's the ground truth? Need to add to the dataset
