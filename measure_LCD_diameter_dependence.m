541fdb9a97290dc061a19b8dd481a6f72e97daf3
Brandon Nelson <bnel1201@gmail.com>
2023-06-28 06:52:25 -0700

    finished processing all diameters

--- measure_LCD_diameter_dependence.m
+++ measure_LCD_diameter_dependence.m
@@ -11,6 +11,17 @@ observers = {LG_CHO_2D()};
 %              GABOR_CHO_2D(),...
 %              };
 
+%% Select datasets
+base_directory = 'data';
+series_1.name = 'fbp';
+series_1.dir = fullfile(base_directory, 'CCT189_peds');
+
+series_2.name = 'Simple CNN MSE'
+series_2.dir  = fullfile(base_directory, 'CCT189_peds_denoised');
+
+
+series_list = [series_1, series_2];
+
 ground_truth_filename = fullfile(base_data_folder, 'ground_truth.mhd');
 offset = 1000;
 if ~exist(ground_truth_filename, 'file')
@@ -19,7 +30,14 @@ if ~exist(ground_truth_filename, 'file')
 end
 ground_truth = mhd_read_image(ground_truth_filename) - offset;
 
-
-res_table = measure_LCD_vs_diameter(base_data_folder, observers, ground_truth, offset)
+for i = 1:length(series_list)
+  series = series_list(i);
+  res = measure_LCD_vs_diameter(series.dir, observers, ground_truth, offset);
+  if i==1
+      res_table = res;
+  end
+  res_table = cat(1, res_table, res);
+      
+end
 write_lcd_results(res_table, 'lcd_v_diameter_results.csv')
 % where's the ground truth? Need to add to the dataset
