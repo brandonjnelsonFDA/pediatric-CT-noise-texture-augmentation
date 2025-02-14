2864a02287d2a6a97a70c9f51b091ebb521f1271
Brandon J Nelson <brandon.nelson@fda.hhs.gov>
2025-02-13 20:18:22 -0500

    mixing models and datasets (unet, redcnn | mayo ldgc, head sims) using lightning, cool!

--- head_denoising/run_training.sh
+++ head_denoising/run_training.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-INPUT=${1:-input.csv}
+CONFIG=${1:-'mayo_ldgc_unet.yaml'}
+COUNT=${2:-1} #number of models to train (default 1)
+
 SIM_NAME=peds_noise_augmentation_$(date +'%Y-%m-%d_%H-%M')
 LOG_DIR=logs/$SIM_NAME
-DATA_DIR='/projects01/didsr-aiml/brandon.nelson/pedsilicoICH/head_experiment/'
-COUNT=3
 
 echo Training $COUNT Model[s]
 
 START_TASK=1
 END_TASK=$COUNT
-qsub -N $SIM_NAME -t $START_TASK-$END_TASK train_model.sge $LOG_DIR $DATA_DIR $COUNT
+qsub -N $SIM_NAME -t $START_TASK-$END_TASK train_model.sge $LOG_DIR $CONFIG
 
-tensorboard --logdir lightning_logs/
+# tensorboard --logdir lightning_logs/
